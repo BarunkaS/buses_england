3aec88c0d242f0191ecdebb2abc11a855824e7cb
BarunkaS <barbora.spacilova@gmail.com>
2021-08-03 16:26:17 +0100

    Create source_locations.py

---
+++ source_locations.py
@@ -0,0 +1,22 @@
+import urllib.request
+import json
+import pandas as pd
+import os
+
+
+# API key
+bus_api_key = os.environ.get('BUS_TRACKING_APIKEY')
+
+
+# APIs
+locations_api = 'https://data.bus-data.dft.gov.uk/api/v1/datafeed/3439/?api_key='+str(bus_api_key)
+
+t=2880
+
+#for i in range(0,t):
+response = urllib.request.urlopen(locations_api)
+location_raw = pd.read_xml(response)
+
+print(location_raw)
+
+
